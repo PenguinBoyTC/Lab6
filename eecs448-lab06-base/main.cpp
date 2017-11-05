de4bd48d688650d1289c2b1ee41f0275ade4c7d7
Chong Tan <a46981@gmail.com>
2017-11-04 21:30:30 -0500

    Finished the all test

--- eecs448-lab06-base/main.cpp
+++ eecs448-lab06-base/main.cpp
@@ -11,14 +11,19 @@
 int main(int argc, char** argv)
 {
 	//Example of declaring a LinkedListOfInts
-	LinkedListOfInts testableList;
+	// LinkedListOfInts testableList;
 
 	//You won't do all the tests in main; that's what your Test class will be for
 	//Example:
+	std::cout << "==================================================================================================\n";
+	std::cout << "------Start Testing------\n";
+	std::cout << "==================================================================================================\n";
+	std::cout << "Running...\n";
 	TestSuite myTester;
+	myTester.setTestSize(50);
 	myTester.runTests();
 
-	std::cout << "Running...\nAnd we're done.\nGoodbye.\n";
+	std::cout << "And we're done.\nGoodbye.\n";
 	
 	return (0);
 
